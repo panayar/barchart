a2583d295cad8691b0e1c2b5b9722bd73d1bcaa7
Paula <panaya@unbosque.edu.co>
2022-07-17 14:53:46 -0500

    uploading the barchart component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import BarChart from "./Components/BarChart";
+import { useState } from "react";
+
+import {UserData} from "./Data/Data";
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map(user => user.year),
+    datasets: [{
+      label:"Users Gain",
+      data: UserData.map(user => user.userGain),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1
+    }, 
+  ]
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BarChart chartData={userData} />
     </div>
   );
 }
---
+++ src/Data/Data.js
@@ -0,0 +1,36 @@
+export const UserData  = [
+    {
+        "id": 1,
+        "year": 2010,
+        "userGain": 80000, 
+        "userLost": 100,
+    }
+    ,
+    {
+        "id": 2,
+        "year": 2011,
+        "userGain": 70000,
+        "userLost": 300,
+    }
+    ,
+    {
+        "id": 3,
+        "year": 2012,
+        "userGain": 4000,
+        "userLost": 100,
+    }
+    ,
+    {
+        "id": 4,
+        "year": 2015,
+        "userGain": 60000,
+        "userLost": 200,
+    },
+    {
+        "id": 5,
+        "year": 2020,
+        "userGain": 30000,
+        "userLost": 50,
+    }
+
+]
